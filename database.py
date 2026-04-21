87516f705a163730fc7f11c72f1cd9f8fedd1139
Andres Monsalve <andresfelipemope@ufps.edu.co>
2026-04-21 13:04:43 -0500

    feat: conexión a Cloud SQL funcionando + ignore de archivos sensibles

--- database.py
+++ database.py
@@ -1,16 +1,21 @@
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Configuración de Cloud SQL
 DB_HOST = os.getenv("DB_HOST", "")
 DB_NAME = os.getenv("DB_NAME", "")
 DB_USER = os.getenv("DB_USER", "")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+print("DB_HOST:", DB_HOST)
 
 def get_db_connection():
     return psycopg2.connect(
         host=DB_HOST,
+        port=os.getenv("DB_PORT", 5432),
         database=DB_NAME,
         user=DB_USER,
         password=DB_PASSWORD,
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from database import get_db_connection
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import os 
 
 load_dotenv()
 app = FastAPI()
 
+class Student(BaseModel):
+    name: str
+    age: int
 
 @app.get("/")
 def read_root():
@@ -20,3 +24,21 @@ def read_students():
     cursor.close()
     db.close()
     return {"students": students}
+
+@app.post("/students")
+def create_students(student: Student):
+    db = get_db_connection()
+    cursor = db.cursor()
+    
+    cursor.execute(
+        "INSERT INTO students (name, age) VALUES (%s, %s) RETURNING *",
+        (student.name, student.age)
+    )
+    
+    new_student = cursor.fetchone()
+    
+    db.commit()
+    cursor.close()
+    db.close()
+    
+    return{"student": new_student}
